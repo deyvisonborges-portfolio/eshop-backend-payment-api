898b6b087bb56eca4bc63369faec2ead6a34af6d
deyvisonborges <deyvisonsilvaborges@gmail.com>
2024-04-11 09:40:45 -0300

    add logger into repository and create payment use case

--- src/main/java/com/eshop/backendpaymentapi/app/persistence/payment/PaymentJPARepository.java
+++ src/main/java/com/eshop/backendpaymentapi/app/persistence/payment/PaymentJPARepository.java
@@ -3,12 +3,17 @@ package com.eshop.backendpaymentapi.app.persistence.payment;
 import com.eshop.backendpaymentapi.core.artifacts.payment.Payment;
 import com.eshop.backendpaymentapi.core.artifacts.payment.repository.PaymentRepositoryContract;
 import com.eshop.backendpaymentapi.lib.domain.Identifier;
+import com.eshop.backendpaymentapi.lib.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.text.MessageFormat;
 import java.util.Optional;
 
 @Service
 public class PaymentJPARepository implements PaymentRepositoryContract {
+  private final Logger logger = LoggerFactory.getLogger(PaymentJPARepository.class);
   private final PaymentJPARepository repository;
 
 	public PaymentJPARepository(PaymentJPARepository repository) {
@@ -17,7 +22,12 @@ public class PaymentJPARepository implements PaymentRepositoryContract {
 
 	@Override
   public void save(Payment payment) {
-    this.repository.save(payment);
+    try {
+      this.repository.save(payment);
+    } catch (Exception e) {
+      this.logger.info("Repository -> Error while saving payment", e);
+      throw e;
+    }
   }
 
   @Override
@@ -27,12 +37,23 @@ public class PaymentJPARepository implements PaymentRepositoryContract {
 
   @Override
   public void delete(Identifier id) {
-    final var payment = this.repository.findById(id);
-    this.repository.delete(payment.get().getId());
+    try {
+      final var payment = this.repository.findById(id);
+      this.repository.delete(payment.get().getId());
+    } catch (Exception e) {
+      this.logger.info(MessageFormat.format("Error while delete Payment with id: {0}", id), e);
+      throw e;
+    }
   }
 
   @Override
   public Optional<Payment> findById(Identifier id) {
-    return Optional.empty();
+    try {
+      return Optional.ofNullable(this.repository.findById(id)
+        .orElseThrow(() -> new NotFoundException(MessageFormat.format("Not found Payment with id: {0}", id))));
+    } catch (Exception e) {
+      this.logger.info(MessageFormat.format("Error while fetching Payment with id: {0}", id), e);
+      throw e;
+    }
   }
 }
--- src/main/java/com/eshop/backendpaymentapi/core/artifacts/payment/usecase/create/CreatePaymentHandler.java
+++ src/main/java/com/eshop/backendpaymentapi/core/artifacts/payment/usecase/create/CreatePaymentHandler.java
@@ -3,8 +3,12 @@ package com.eshop.backendpaymentapi.core.artifacts.payment.usecase.create;
 import com.eshop.backendpaymentapi.core.artifacts.payment.Payment;
 import com.eshop.backendpaymentapi.core.artifacts.payment.repository.PaymentRepositoryContract;
 import com.eshop.backendpaymentapi.core.artifacts.payment.usecase.UseCaseContract;
+import com.eshop.backendpaymentapi.lib.exception.BadRequestException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CreatePaymentHandler extends UseCaseContract<CreatePaymentCommand, CreatePaymentOutput> {
+  private final Logger logger = LoggerFactory.getLogger(CreatePaymentHandler.class);
 	private final PaymentRepositoryContract repository;
 
 	public CreatePaymentHandler(PaymentRepositoryContract repository) {
@@ -13,17 +17,22 @@ public class CreatePaymentHandler extends UseCaseContract<CreatePaymentCommand, 
 
 	@Override
 	public CreatePaymentOutput execute(final CreatePaymentCommand command) {
-		final var value = command.value();
-		final var status = command.status();
-		final var method = command.method();
-    final var paidIn = command.paidIn();
-		final var orderId = command.orderId();
-		final var customerId = command.customerId();
+		try {
+      final var value = command.value();
+      final var status = command.status();
+      final var method = command.method();
+      final var paidIn = command.paidIn();
+      final var orderId = command.orderId();
+      final var customerId = command.customerId();
 
-    final var payment = Payment.factory(value, status, method, paidIn, orderId, customerId);
+      final var payment = Payment.factory(value, status, method, paidIn, orderId, customerId);
 
-    this.repository.save(payment);
+      this.repository.save(payment);
 
-    return CreatePaymentOutput.from(payment);
+      return CreatePaymentOutput.from(payment);
+    } catch (Exception e) {
+      this.logger.info("Error while saving the payment", e);
+      throw new BadRequestException(e.getMessage());
+    }
 	}
 }
